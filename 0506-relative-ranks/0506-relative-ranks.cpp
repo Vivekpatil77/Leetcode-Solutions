a515a7b8324ed7919fa87f0237ea8aa7743e5234
Vivek Patil <viveknp0077@gmail.com>
2026-07-31 19:02:19 +0530

    Time: 6 ms (45.07%), Space: 15.4 MB (78.67%) - LeetHub

---
+++ 0506-relative-ranks/0506-relative-ranks.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<string> findRelativeRanks(vector<int>& score) {
+        priority_queue<pair<int,int>> pq;
+        int n = score.size();
+        for(int i =0;i<n;i++)
+        {
+            pq.push({score[i],i});
+        }
+        vector<string> ans(n);
+        int rank =1;
+        while(!pq.empty())
+
+        {
+            int index = pq.top().second;
+            pq.pop();
+            
+           if(rank == 1)
+                ans[index] = "Gold Medal";
+            else if(rank == 2)
+                ans[index] = "Silver Medal";
+            else if(rank == 3)
+                ans[index] = "Bronze Medal";
+            else
+                ans[index] = to_string(rank);
+
+            rank++;
+
+        }
+        return ans;
+        
+    }
+};
